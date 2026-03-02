c6f5484d35e6bdf8e390b03e519e4e92db19e664
Naman <namangarg20021128@gmail.com>
2026-03-02 16:25:27 +0530

    deployment config added

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -7,9 +8,24 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.get('/', (req, res) => {
+const __dirname = path.resolve();
+
+app.get('/health', (req, res) => {
     res.status(200).json({ message: 'successs from backend API' });
 });
+
+app.get('/books', (req, res) => {
+    res.status(200).json({ message: 'this is the books endpoint' });
+});
+
+// make our app ready for deployment
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    app.get('/{*any}', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
