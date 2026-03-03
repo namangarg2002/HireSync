c080569bea467b1c3b66268046e78247064c8785
Naman <98084335+namangarg2002@users.noreply.github.com>
2026-03-03 16:56:41 +0530

    Update import paths to include .js extension

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,6 +1,6 @@
 import { Inngest } from "inngest";
-import connectDB from "./db";
-import User from "../models/User";
+import connectDB from "./db.js";
+import User from "../models/User.js";
 
 export const inngest = new Inngest({ id: "HireSync" });
 
