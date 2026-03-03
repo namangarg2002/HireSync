7685280c4699a163fd571a6d785831dd4e1cb2a4
Naman <namangarg20021128@gmail.com>
2026-03-03 17:09:20 +0530

    more chamges

--- backend/index.js
+++ backend/index.js
@@ -30,10 +30,10 @@ app.get('/books', (req, res) => {
 
 // make our app ready for deployment
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+    app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
-    app.get('/{*any}', (req, res) => {
-        res.sendFile(path.join(__dirname, '/frontend/dist/index.html'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'frontend/dist/index.html'));
     });
 }
 
