03cbb3c26e1472cb872d09c3a95b912d451716c7
Naman <namangarg20021128@gmail.com>
2026-03-09 18:47:11 +0530

    problems-page

---
+++ frontend/src/lib/utils.js
@@ -0,0 +1,15 @@
+const getDifficultyBadgeClass = (difficulty) => {
+    switch(difficulty.toLowerCase()){
+        case 'easy':
+            return 'badge-success';
+        case 'medium':
+            return 'badge-warning';
+        case 'hard':
+            return 'badge-error';
+
+        default:
+            return 'badge-ghost'
+    }
+}
+
+export default getDifficultyBadgeClass;
